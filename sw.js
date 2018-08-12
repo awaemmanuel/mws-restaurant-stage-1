6bf8232d11bf1aa54d2c0f10294800bef29d09bc
Emmanuel Awa <eawa@brandeis.edu>
2018-08-12 13:13:23 -0400

    Updating console.log

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener("install", event => {
 self.addEventListener('fetch', (event) => {
   // skip cross-origin requests
   if (event.request.url.startsWith(self.location.origin)) {
-    console.log('Service worker fetch...', self.origin.location);
+    console.log('Service worker fetch...', self.location.origin);
     event.respondWith(
       caches.match(event.request)
         .then((cachedResponse) => {
